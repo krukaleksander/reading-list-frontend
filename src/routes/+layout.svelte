e69ee555886d7bfff2ce82ab747bf31ae9406d25
krukaleksander <kruk.aleksander@hotmail.com>
2024-05-24 14:16:48 +0200

    chore: style service status

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,7 @@
   const { healthCheckResponse } = data;
 </script>
 
-<p>API Status: {healthCheckResponse}</p>
+<div class="flex justify-end">
+    <p class="p-4 m-3 bg-white rounded shadow-md">API Status: <span class="text-green-600 font-bold">{healthCheckResponse}</span></p>
+</div>
 <slot />
