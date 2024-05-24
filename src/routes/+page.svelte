f6c0b33130d6c1bdb1aa8c093b9c4df2b0d66111
krukaleksander <kruk.aleksander@hotmail.com>
2024-05-24 14:10:11 +0200

    feat: add login form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,28 @@
-<h1 class="text-3xl font-bold text-center">Provide credentials</h1>
+<div class="flex items-center justify-center min-h-screen bg-gray-200">
+  <div class="p-6 m-3 bg-white rounded shadow-md w-80">
+    <h2 class="mb-4 text-xl font-bold text-center">Login</h2>
+    <div class="mb-4">
+      <label class="block mb-2 text-sm font-bold text-gray-700" for="username">
+        Username
+      </label>
+      <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label class="block mb-2 text-sm font-bold text-gray-700" for="password">
+        Password
+      </label>
+      <input class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline" type="button">
+        Log in
+      </button>
+    </div>
+  </div>
+</div>
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.red.100);
+    background-color: theme(colors.gray.200);
   }
 </style>
