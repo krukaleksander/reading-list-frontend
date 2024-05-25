dc2c125d299fe32609d501f0cd40dd0dd4ef6206
krukaleksander <kruk.aleksander@hotmail.com>
2024-05-25 14:07:55 +0200

    feat: create login function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
+<script>
+  let username = "";
+  let password = "";
+  let getAllResponse;
+
+  const login = async () => {
+    const token = btoa(`${username}:${password}`);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/all`, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
+    });
+   getAllResponse = await response.json();
+  };
+</script>
+
 <div class="flex items-center justify-center min-h-screen">
   <div class="p-6 m-3 bg-white rounded shadow-md w-80">
     <h2 class="mb-4 text-xl font-bold text-center">Login</h2>
@@ -6,6 +23,7 @@
         Username
       </label>
       <input
+        bind:value={username}
         class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
         id="username"
         type="text"
@@ -17,6 +35,7 @@
         Password
       </label>
       <input
+        bind:value={password}
         class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
         id="password"
         type="password"
@@ -25,6 +44,7 @@
     </div>
     <div class="flex items-center justify-between">
       <button
+        on:click={login}
         class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
         type="button"
       >
@@ -33,5 +53,3 @@
     </div>
   </div>
 </div>
-
-
